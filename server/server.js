b2494bd65d75d338bff6cad2563ffe0ff79d04ee
David Schwartz <davidphillipschwartz@gmail.com>
2018-08-22 13:06:00 -0700

    basic twilio notifications

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
+let fs = require('fs')
 let express = require('express')
 let moment = require('moment')
 let bodyParser = require('body-parser')
 let app = express()
 let jsonBodyParser = bodyParser.json()
-
+let config = JSON.parse(fs.readFileSync('brave_config.json', 'utf8'))
+let twilioClient = require('twilio')(config.TWILIO_ACCOUNT_SID, config.TWILIO_AUTH_TOKEN)
 let lastHeartbeatTime = moment()
+
+function sendAlerts() {
+    for(let i=0; i<config.TWILIO_TO_NUMBERS.length; i++) {
+        twilioClient.messages.create({
+            body: 'The Flic connection has been lost.',
+            from: config.TWILIO_FROM_NUMBER,
+            to: config.TWILIO_TO_NUMBERS[i]
+        })
+        .then(message => console.log(message.sid))
+        .done()
+    }
+}
 
 app.post('/heartbeat', jsonBodyParser, (req, res) => {
     console.log('got a heartbeat, flic_ok is ' + req.body.flic_ok.toString())
@@ -19,6 +33,7 @@ function checkHeartbeat() {
     let heartbeatDelayMillis = currentTime.diff(lastHeartbeatTime)
     if(heartbeatDelayMillis > 70000) {
         console.log('lost connection to flic!')
+        sendAlerts()
     }
 }
 
