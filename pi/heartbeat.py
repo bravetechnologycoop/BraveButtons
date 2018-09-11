49b5531375f5a906058ff157922eb690c794a05c
David Schwartz <davidphillipschwartz@gmail.com>
2018-09-11 21:25:44 +0000

    heartbeat script controls flic hub based on status of server connection

--- pi/heartbeat.py
+++ pi/heartbeat.py
@@ -42,24 +42,40 @@ def send_heartbeat(flic_ok):
     if flic_ok:
         body = '{"flic_ok":true}'
     headers = {'Content-Type':'application/json'}
-    conn = http.client.HTTPSConnection(SERVER_URL)
-    conn.request('POST', r'/heartbeat', body, headers)
-    res = conn.getresponse()
-    print('sent heartbeat, got response: ', res.status, res.reason)
+    try:
+        conn = http.client.HTTPSConnection(SERVER_URL)
+        conn.request('POST', r'/heartbeat', body, headers)
+        res = conn.getresponse()
+        print('sent heartbeat, got response: ', res.status, res.reason)
+        if res.status == 200:
+            return True
+        return False
+    except:
+        return False
 
 if __name__ == '__main__':
+    
+    # wait for reboot to finish
+    time.sleep(10)
+
+    print("starting heartbeat script")
 
     relay = gpiozero.OutputDevice(GPIO_PIN)
     relay.on()
 
     while True: 
+        server_connection_ok = False
         try:
             num_secs = parse_darkstat_html_lines(get_darkstat_html().splitlines())
             if num_secs < 70:
-                send_heartbeat(True)
+                server_connection_ok = send_heartbeat(True)
             else:
-                send_heartbeat(False)
-        except ValueError:
-            print "ValueError while parsing darkstat html or sending heartbeat"
+                server_connection_ok = send_heartbeat(False)
+        except ValueError as e:
+            print(e)
         finally:
-            time.sleep(1)
+            if server_connection_ok:
+                relay.on()
+            else:
+                relay.off()
+            time.sleep(5)
