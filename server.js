8f372ba5f19978c7aa6067da5e18dc84a877cebb
Yana Pertels <ypertels@gmail.com>
2018-10-22 23:55:41 -0700

    more tests

--- server.js
+++ server.js
@@ -15,15 +15,31 @@ function log(logString) {
 
 app.post('/', jsonBodyParser, (req, res) => {
 
-	
-    log('UUID: ' + req.body.UUID.toString() + ' Unit:' + req.body.Unit.toString())
-    res.status(200).send()
+	if (req.body.hasOwnProperty('UUID') && req.body.hasOwnProperty('Unit')) {
+
+	    log('UUID: ' + req.body.UUID.toString() + ' Unit:' + req.body.Unit.toString());
+	    res.status(200).send();
+	    
+	} else {
+		log('Bad request: request does not contain button ID or unit');
+		res.status(400).send();
+	}
 })
 
-let httpsOptions = {
-    key: fs.readFileSync(`/etc/letsencrypt/live/chatbot.brave.coop/privkey.pem`),
-    cert: fs.readFileSync(`/etc/letsencrypt/live/chatbot.brave.coop/fullchain.pem`)
+
+let server;
+
+if (require.main === module) {   // if called directly, start the real server
+	let httpsOptions = {
+	    key: fs.readFileSync(`/etc/letsencrypt/live/chatbot.brave.coop/privkey.pem`),
+	    cert: fs.readFileSync(`/etc/letsencrypt/live/chatbot.brave.coop/fullchain.pem`)
+	}
+
+	server = https.createServer(httpsOptions, app).listen(443)
+	log('brave server listening on port 443')
+} else {
+	server = app.listen(443);
+
 }
 
-https.createServer(httpsOptions, app).listen(443)
-log('brave server listening on port 443')
+module.exports = server;
--- test/serverTest.js
+++ test/serverTest.js
@@ -1,5 +1,6 @@
 let nock = require('nock');
 let chai = require('chai');
+let app = require('../server.js');
 let chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const expect = chai.expect;
@@ -12,10 +13,25 @@ describe('Chatbot server', () => {
 		'UUID': '111'
 	};
 
+	let defaultBody2 = {
+		'Unit': '222',
+		'UUID': '222'
+	};
+
 	describe('POST request', () => {
 		it('should return ok to a valid request', async () => {
-			let response = await chai.request(baseUrl).post('/') .send(defaultBody);
+			let response = await chai.request(app).post('/') .send(defaultBody);
 			expect(response).to.have.status(200);
+		});
+
+	    it('should return 400 to a request with an empty body', async () => {
+			let response = await chai.request(app).post('/') .send({});
+			expect(response).to.have.status(400);
+		});
+
+		it('should return 400 to a request with an incomplete body', async () => {
+			let response = await chai.request(app).post('/') .send({'Unit': '400'});
+			expect(response).to.have.status(400);
 		});
 	});
 });
