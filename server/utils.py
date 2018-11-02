74206a9a0d189fd4da8eb5e1e721b15fdb9981cc
David Schwartz <davidphillipschwartz@gmail.com>
2018-11-01 20:07:51 -0700

    added heartbeat test function to server utils

--- server/utils.py
+++ server/utils.py
@@ -8,3 +8,13 @@ def send_rename_request(server_url, system_id, name):
     except Exception as e:
         print("error sending rename request")
         print(e)
+
+def send_heartbeat(server_url, system_id, flic_last_seen_secs):
+    payload = {"system_id": system_id, "flic_last_seen_secs": flic_last_seen_secs}
+    try:
+        r = requests.post(server_url + r"/heartbeat", json = payload)
+        print("response to heartbeat:", r.status_code, r.reason)
+    except Exception as e:
+        print("error sending heartbeat")
+        print(e)
+
