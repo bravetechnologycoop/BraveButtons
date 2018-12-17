24d87b92aa91686fbac24cc046340c7e0e51a37c
David Schwartz <davidphillipschwartz@gmail.com>
2018-12-16 17:37:46 -0800

    set up autossh in pi setup script

--- pi/setup_pi.sh
+++ pi/setup_pi.sh
@@ -9,7 +9,7 @@ if [[ $EUID > 0 ]]; then
 
 else
   apt-get update
-  apt-get install -y darkstat bridge-utils python3-gpiozero
+  apt-get install -y darkstat bridge-utils python3-gpiozero autossh ssh
   pip3 install python-daemon
 
   cat "$BASEDIR/darkstat_init.txt" > /etc/darkstat/init.cfg
@@ -25,12 +25,25 @@ else
   chown pi:pi /usr/local/brave /usr/local/brave/system_id
   chmod 700 /usr/local/brave
 
-  systemd_unit_file=$(<$BASEDIR/systemd_unit_file.txt)
-  systemd_unit_file="${systemd_unit_file//HEARTBEAT_SCRIPT_PATH/$(pwd)/$BASEDIR/heartbeat.py}"
-  echo "$systemd_unit_file" > /etc/systemd/system/brave-heartbeat.service
+  mkdir /home/pi/.ssh
+  ssh-keygen -t rsa -N "" -f /home/pi/.ssh/id_rsa
+  echo "SSH public key:\n"
+  cat /home/pi/.ssh/id_rsa.pub
+  read -p "\nplease copy the SSH public key to the remote access server. press [Enter] when you are finished.\n"
+  echo "please enter the port number to use for remote SSH access to this device:"
+  read remoteAccessPort
+
+  autossh_systemd_unit_file=$(<$BASEDIR/autossh_systemd_unit_file.txt)
+  autossh_systemd_unit_file="${autossh_systemd_unit_file//REMOTE_ACCESS_PORT/$remoteAccessPort}"
+  echo "$autossh_systemd_unit_file" > /etc/systemd/system/brave-autossh.service
+
+  heartbeat_systemd_unit_file=$(<$BASEDIR/heartbeat_systemd_unit_file.txt)
+  heartbeat_systemd_unit_file="${heartbeat_systemd_unit_file//HEARTBEAT_SCRIPT_PATH/$(pwd)/$BASEDIR/heartbeat.py}"
+  echo "$heartbeat_systemd_unit_file" > /etc/systemd/system/brave-heartbeat.service
 
   systemctl daemon-reload
   systemctl enable brave-heartbeat.service
+  systemctl enable brave-autossh.service
   systemctl enable darkstat
 
   echo "setup almost complete. rebooting..."
