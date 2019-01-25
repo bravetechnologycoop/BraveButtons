0281406bd0f7faa5fbb606f7cfd84fac9b3fd924
David Schwartz <davidphillipschwartz@gmail.com>
2019-01-24 16:35:23 -0800

    pi install script reads config file instead of asking for interactive input

--- pi/setup_pi.sh
+++ pi/setup_pi.sh
@@ -6,8 +6,26 @@ BASEDIR=$(dirname "$0")
 if [[ $EUID > 0 ]]; then
   echo "this script needs sudo privelages to run correctly."
   exit 1
-
+elif [[ ! -e "$BASEDIR/$1" ]]; then
+  echo "please supply the path to the config file as the first argument when running this script."
+  exit 1
 else
+
+  while IFS="=" read -r name value; do
+    if [[ "$name" == "hostname" ]]; then
+      hostname="$value"
+    elif [[ "$name" == "remoteAccessPort" ]]; then
+      remoteAccessPort="$value"
+    elif [[ "$name" == "remoteAccessServerFQDN" ]]; then
+      remoteAccessServerFQDN="$value"
+    fi
+  done < $1
+
+  if [[ ! -v hostname ]] || [[ ! -v remoteAccessPort ]] || [[ ! -v remoteAccessServerFQDN ]]; then
+    echo "the configuration file for this script is invalid."
+    exit 1
+  fi
+
   apt-get update
   apt-get install -y darkstat bridge-utils python3-gpiozero autossh ssh
   pip3 install python-daemon
@@ -25,9 +43,6 @@ else
   chown pi:pi /usr/local/brave /usr/local/brave/system_id
   chmod 700 /usr/local/brave
 
-  echo "please enter the hostname to use for this device:"
-  read hostname
-  
   hosts_file=$(</etc/hosts)
   hosts_file="${hosts_file//raspberrypi/$hostname}"
   echo "$hosts_file" > /etc/hosts
@@ -39,11 +54,10 @@ else
   echo "SSH public key:"
   cat /home/pi/.ssh/id_rsa.pub
   read -p "please copy the SSH public key to the remote access server. press [Enter] when you are finished."
-  echo "please enter the port number to use for remote SSH access to this device:"
-  read remoteAccessPort
 
   autossh_systemd_unit_file=$(<$BASEDIR/autossh_systemd_unit_file.txt)
   autossh_systemd_unit_file="${autossh_systemd_unit_file//REMOTE_ACCESS_PORT/$remoteAccessPort}"
+  autossh_systemd_unit_file="${autossh_systemd_unit_file//REMOTE_ACCESS_SERVER_FQDN/$remoteAccessServerFQDN}"
   echo "$autossh_systemd_unit_file" > /etc/systemd/system/brave-autossh.service
 
   heartbeat_systemd_unit_file=$(<$BASEDIR/heartbeat_systemd_unit_file.txt)
