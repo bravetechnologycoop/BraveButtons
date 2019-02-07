c87ca7f23c930107c7fc902fa77b990f9b01b64b
Theresa Deering <trisapeace@gmail.com>
2019-02-06 16:58:28 -0800

    Change paths in tests to be relative to root
    - So that we can run pytest successfully from the root of the repo

--- pi/test_heartbeat.py
+++ pi/test_heartbeat.py
@@ -6,17 +6,17 @@ import socket
 class Test__parse_darkstat_html_lines(object):
 
     def test_seconds(self):
-        with open('./sample_darkstat_html/59_secs.html', 'r') as html_file:
+        with open('./pi/sample_darkstat_html/59_secs.html', 'r') as html_file:
             html = html_file.read()
             assert heartbeat.parse_darkstat_html_lines(html.splitlines()) == 59
 
     def test_seconds_minutes(self):
-        with open('./sample_darkstat_html/316_secs.html', 'r') as html_file:
+        with open('./pi/sample_darkstat_html/316_secs.html', 'r') as html_file:
             html = html_file.read()
             assert heartbeat.parse_darkstat_html_lines(html.splitlines()) == 316
 
     def test_seconds_minutes_hours(self):
-        with open('./sample_darkstat_html/3806_secs.html', 'r') as html_file:
+        with open('./pi/sample_darkstat_html/3806_secs.html', 'r') as html_file:
             html = html_file.read()
             assert heartbeat.parse_darkstat_html_lines(html.splitlines()) == 3806
 
@@ -26,7 +26,7 @@ class Test__parse_darkstat_html_lines(object):
 
     def test_never(self):
         with pytest.raises(heartbeat.FlicNotFoundError):
-            with open('./sample_darkstat_html/never.html', 'r') as html_file:
+            with open('./pi/sample_darkstat_html/never.html', 'r') as html_file:
                 html = html_file.read()
                 heartbeat.parse_darkstat_html_lines(html.splitlines())
 
