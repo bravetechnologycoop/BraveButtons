0fe0d1bbf1e130d1d569409404209961dd152d1d
David Schwartz <davidphillipschwartz@gmail.com>
2019-02-13 13:15:32 -0800

    updated chatbot message text

--- SessionState.js
+++ SessionState.js
@@ -71,27 +71,27 @@ class SessionState {
         switch (this.state) {
             case STATES.STARTED:
                 this.state = STATES.WAITING_FOR_CATEGORY;
-                returnMessage = 'Thank you for responding.\n Please reply with the number that best describes the nature of the incident\n0 - accidental\n1 - safer use\n2 - unsafe guest\n3 - overdose';
+                returnMessage = 'Now that you have responded, please reply with the number that best describes the incident:\n0 - accidental\n1 - safer use\n2 - unsafe guest\n3 - overdose';
                 break;
             case STATES.WAITING_FOR_REPLY:
                 this.state = STATES.WAITING_FOR_CATEGORY;
-                returnMessage = 'Thank you for responding.\n Please reply with the number that best describes the nature of the incident\n0 - accidental\n1 - safer use\n2 - unsafe guest\n3 - overdose';
+                returnMessage = 'Now that you have responded, please reply with the number that best describes the incident:\n0 - accidental\n1 - safer use\n2 - unsafe guest\n3 - overdose';
                 break;
             case STATES.WAITING_FOR_CATEGORY:
                 let isValid = this.setIncidentType(messageText.trim());
                 this.state = isValid ? STATES.WAITING_FOR_DETAILS : STATES.WAITING_FOR_CATEGORY;
-                returnMessage = this.setIncidentType(messageText.trim()) ? 'Thank you. Please add any further details about the incident or comment about this interface.' : 'Sorry, the incident type wasn\'t recognized. Please try again';
+                returnMessage = this.setIncidentType(messageText.trim()) ? 'Thank you. If you like, you can reply with any further details about the incident.' : 'Sorry, the incident type wasn\'t recognized. Please try again.';
                 break;
             case STATES.WAITING_FOR_DETAILS:
                 this.notes = messageText.trim();
                 this.state = STATES.COMPLETED;
-                returnMessage = 'Thank you.';
+                returnMessage = "Thank you. This session is now complete. (You don't need to respond to this message.)";
                 break;
             case STATES.COMPLETED:
-                returnMessage = 'There is no active session for this button.';
+                returnMessage = "There is no active session for this button. (You don't need to respond to this message.)";
                 break;
             case STATES.TIMED_OUT:
-                returnMessage = 'There is no active session for this button.';
+                returnMessage = "There is no active session for this button. (You don't need to respond to this message.)";
                 break;
             default:
                 returnMessage = 'Thank you for responding. Unfortunately, we have encountered an error in our system and will deal with it shortly.';
