ffb6001039460cc233c29fd5a725e2c380fab076
David Schwartz <davidphillipschwartz@gmail.com>
2019-02-19 16:43:59 -0800

    postgres manages created_at and updated_at

--- db/setup.sql
+++ db/setup.sql
@@ -6,8 +6,8 @@ CREATE TABLE IF NOT EXISTS sessions (
     state text NOT NULL,
     num_presses int NOT NULL,
     responded_to boolean NOT NULL DEFAULT FALSE,
-    created_at timestamptz NOT NULL,
-    updated_at timestamptz NOT NULL, 
+    created_at timestamptz NOT NULL DEFAULT NOW(),
+    updated_at timestamptz NOT NULL DEFAULT NOW(), 
     incident_type text,
     notes text
 );
@@ -16,5 +16,24 @@ CREATE TABLE IF NOT EXISTS registry (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     button_id text UNIQUE NOT NULL,
     unit text NOT NULL,
-    phone_number text NOT NULL
+    phone_number text NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT NOW(),
+    updated_at timestamptz NOT NULL DEFAULT NOW()
 );
+
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_sessions_timestamp
+BEFORE UPDATE ON sessions
+FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp;
+
+CREATE TRIGGER set_registry_timestamp
+BEFORE UPDATE ON registry
+FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp;
+
