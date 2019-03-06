923bb9b476f20d0a28ed377fed38e09f703d7544
Brave <brave@Veras-MacBook-Pro.local>
2019-03-06 15:25:58 -0800

    Change postgres setup to work on a Mac and on Travis
    - We ran into this problem https://stackoverflow.com/questions/50993655/how-to-run-a-postgres-command-could-not-identify-current-directory when trying to run the setup DB script
    - Also happens on Travis
    - Followed the accepted answer on that StackOverflow page in order to fix it

--- setup_postgresql.sh
+++ setup_postgresql.sh
@@ -1,4 +1,4 @@
     sudo -u postgres psql -c "CREATE ROLE $PG_USER PASSWORD '$PG_PASSWORD' LOGIN"
     sudo -u postgres createdb -O $PG_USER $PG_USER
     sudo -u postgres psql -d $PG_USER -c 'CREATE EXTENSION IF NOT EXISTS "pgcrypto"'
-    sudo -u postgres PGPASSWORD=$PG_PASSWORD psql -U $PG_USER -d $PG_USER -f ./db/setup.sql
+    sudo PGPASSWORD=$PG_PASSWORD psql -U $PG_USER -d $PG_USER -f ./db/setup.sql
