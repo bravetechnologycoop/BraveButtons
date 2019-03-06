3c757267090b58248628768fc4350662dc7e73db
Brave <brave@Veras-MacBook-Pro.local>
2019-03-06 14:55:16 -0800

    Move postgres setup commands into its own script
    - This script will be used by
      - the setup_server.sh script
      - locally when setting up a new local database
      - Travis to setup the continuous integration database
    so it's nicer that it's factored out somewhere

--- setup_server.sh
+++ setup_server.sh
@@ -41,10 +41,7 @@ else
     n stable
     npm install
 
-    sudo -u postgres psql -c "CREATE ROLE $PG_USER PASSWORD '$PG_PASSWORD' LOGIN"
-    sudo -u postgres createdb -O $PG_USER $PG_USER
-    sudo -u postgres psql -d $PG_USER -c 'CREATE EXTENSION IF NOT EXISTS "pgcrypto"'
-    sudo -u postgres PGPASSWORD=$PG_PASSWORD psql -U $PG_USER -d $PG_USER -f ./db/setup.sql
+    ./setup_postgresql.sh
 
     certbot certonly --standalone 
 
