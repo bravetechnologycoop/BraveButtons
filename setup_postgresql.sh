e34fa839680dc21ba1d6280529ad6f32532c7195
David Schwartz <davidphillipschwartz@gmail.com>
2019-07-15 18:20:48 -0700

    updated setup_postgresql.sh to handle initial installation creation

--- setup_postgresql.sh
+++ setup_postgresql.sh
@@ -4,5 +4,19 @@
 
     sudo PGPASSWORD=$PG_PASSWORD psql -U $PG_USER -d $PG_USER -f ./db/001-setup.sql
     sudo PGPASSWORD=$PG_PASSWORD psql -U $PG_USER -d $PG_USER -f ./db/002-addinstallations.sql
+
+    echo "an installation record will now be created and linked with all existing records in the sessions and registry tables."
+    echo "please ensure that no records already exist in the installations table, or use Ctrl-C to exit this script."
+    echo "please enter a name for the installation:"
+    read installationName
+    echo "please enter the responder phone number:"
+    read responderPhone
+    echo "please enter the fallback phone number:"
+    read fallbackPhone
+
+    sudo PGPASSWORD=$PGPASSWORD psql -U $PG_USER -d $PG_USER -c "INSERT INTO installations VALUES (DEFAULT, '$installationName', '$responderPhone', '$fallbackPhone')"
+    sudo PGPASSWORD=$PGPASSWORD psql -U $PG_USER -d $PG_USER -c "UPDATE sessions SET installation_id = (SELECT id FROM installations LIMIT 1)"
+    sudo PGPASSWORD=$PGPASSWORD psql -U $PG_USER -d $PG_USER -c "UPDATE registry SET installation_id = (SELECT id FROM installations LIMIT 1)"
+
     sudo PGPASSWORD=$PG_PASSWORD psql -U $PG_USER -d $PG_USER -f ./db/003-setinstallationconstraints.sql
    
