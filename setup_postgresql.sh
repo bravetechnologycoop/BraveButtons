74d7162caec92afbbfbbc2e0bd500429dc542ebb
David Schwartz <davidphillipschwartz@gmail.com>
2019-07-16 02:07:47 +0000

    updates to existing data during migration don't overwrite updated_at

--- setup_postgresql.sh
+++ setup_postgresql.sh
@@ -14,9 +14,18 @@
     echo "please enter the fallback phone number:"
     read fallbackPhone
 
-    sudo PGPASSWORD=$PGPASSWORD psql -U $PG_USER -d $PG_USER -c "INSERT INTO installations VALUES (DEFAULT, '$installationName', '$responderPhone', '$fallbackPhone')"
-    sudo PGPASSWORD=$PGPASSWORD psql -U $PG_USER -d $PG_USER -c "UPDATE sessions SET installation_id = (SELECT id FROM installations LIMIT 1)"
-    sudo PGPASSWORD=$PGPASSWORD psql -U $PG_USER -d $PG_USER -c "UPDATE registry SET installation_id = (SELECT id FROM installations LIMIT 1)"
+    # turn off the 'updated at' trigger so that the schema-related updates to existing data don't overwrite the more interesting updated_at values
+    sudo PGPASSWORD=$PG_PASSWORD psql -U $PG_USER -d $PG_USER -c "ALTER TABLE sessions DISABLE TRIGGER set_sessions_timestamp"
+    sudo PGPASSWORD=$PG_PASSWORD psql -U $PG_USER -d $PG_USER -c "ALTER TABLE registry DISABLE TRIGGER set_registry_timestamp"
+
+    # update the existing data to the new schema
+    sudo PGPASSWORD=$PG_PASSWORD psql -U $PG_USER -d $PG_USER -c "INSERT INTO installations VALUES (DEFAULT, '$installationName', '$responderPhone', '$fallbackPhone')"
+    sudo PGPASSWORD=$PG_PASSWORD psql -U $PG_USER -d $PG_USER -c "UPDATE sessions SET installation_id = (SELECT id FROM installations LIMIT 1)"
+    sudo PGPASSWORD=$PG_PASSWORD psql -U $PG_USER -d $PG_USER -c "UPDATE registry SET installation_id = (SELECT id FROM installations LIMIT 1)"
+
+    # turn the triggers back on
+    sudo PGPASSWORD=$PG_PASSWORD psql -U $PG_USER -d $PG_USER -c "ALTER TABLE sessions ENABLE TRIGGER set_sessions_timestamp"
+    sudo PGPASSWORD=$PG_PASSWORD psql -U $PG_USER -d $PG_USER -c "ALTER TABLE registry ENABLE TRIGGER set_registry_timestamp"
 
     sudo PGPASSWORD=$PG_PASSWORD psql -U $PG_USER -d $PG_USER -f ./db/003-setinstallationconstraints.sql
    
