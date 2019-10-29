cd8c3989abc277181a94d0493b20f391f33a4e31
MarioCimet <mariocimet@users.noreply.github.com>
2019-10-29 11:18:14 -0700

    fixing bug in how psql user is specified and removing redundant user flag

--- chatbot/setup_postgresql.sh
+++ chatbot/setup_postgresql.sh
@@ -2,6 +2,6 @@ sudo -u postgres psql -c "CREATE ROLE $PG_USER PASSWORD '$PG_PASSWORD' LOGIN"
 sudo -u postgres createdb -O $PG_USER $PG_USER
 sudo -u postgres psql -d $PG_USER -c 'CREATE EXTENSION IF NOT EXISTS "pgcrypto"'
 
-sudo PGPASSWORD=$PG_PASSWORD psql -U $PG_USER -d $PG_USER -f ./db/001-setup.sql
-sudo PGPASSWORD=$PG_PASSWORD psql -U $PG_USER -d $PG_USER -f ./db/002-addinstallations.sql -v installationName="'$1'" -v responderPhone="'$2'" -v fallbackPhone="'$3'"
-sudo PGPASSWORD=$PG_PASSWORD psql -U $PG_USER -d $PG_USER -f ./db/003-setinstallationconstraints.sql
+sudo -u $PG_USER PGPASSWORD=$PG_PASSWORD psql -d $PG_USER -f ./db/001-setup.sql
+sudo -u $PG_USER PGPASSWORD=$PG_PASSWORD psql -d $PG_USER -f ./db/002-addinstallations.sql -v installationName="'$1'" -v responderPhone="'$2'" -v fallbackPhone="'$3'"
+sudo -u $PG_USER PGPASSWORD=$PG_PASSWORD psql -d $PG_USER -f ./db/003-setinstallationconstraints.sql
