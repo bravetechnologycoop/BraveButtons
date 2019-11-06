5b0031d1c3bd3e6b50839e104ed9215a6219f438
David Schwartz <davidphillipschwartz@gmail.com>
2019-11-05 21:29:47 -0800

    fixed cert renewal bug in heartbeat server

--- server/setup_server.sh
+++ server/setup_server.sh
@@ -3,6 +3,7 @@
 set -e
 original_dir=$(pwd)
 cd $(dirname "$0")
+export HOME=$(bash <<< "echo ~$SUDO_USER")
 
 if [[ $EUID > 0 ]]; then
     echo "this script needs sudo privelages to run correctly."
@@ -37,7 +38,7 @@ else
 
     certbot certonly --standalone 
 
-    echo "0 0 * * 0 certbot renew --pre-hook 'pm2 stop server' --post-hook 'pm2 start server'" > crontab.tmp
+    echo "0 0 * * 0 certbot renew --pre-hook 'env HOME=$HOME pm2 stop BraveHeartbeatServer' --post-hook 'env HOME=$HOME pm2 start BraveHeartbeatServer'" > crontab.tmp
     crontab crontab.tmp
     rm crontab.tmp
     
