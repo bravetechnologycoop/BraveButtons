1afdc7ca696c62d6110e72ce164efa11b9b248cd
David Schwartz <davidphillipschwartz@gmail.com>
2020-03-04 15:10:08 -0800

    added simple tests for SessionState_Alvis

--- chatbot/test/SessionStateTest.js
+++ chatbot/test/SessionStateTest.js
@@ -4,18 +4,18 @@ var beforeEach = require('mocha').beforeEach
 var describe = require('mocha').describe
 var it = require('mocha').it
 
-let { SessionState } = require('../SessionState.js');
+let { SessionState, SessionState_Alvis } = require('../SessionState.js');
 const STATES = require('../SessionStateEnum.js');
 
-describe('Session state manager', () => {
+const sessionId = '12345'
+const installationId = '67890'
+const buttonId = '12345'
+const unit = '1'
+const phoneNumber = '+14206666969'
+const createdAt = Date()
+const updatedAt = Date()
 
-    const sessionId = '12345'
-    const installationId = '67890'
-    const buttonId = '12345'
-    const unit = '1'
-    const phoneNumber = '+14206666969'
-    const createdAt = Date()
-    const updatedAt = Date()
+describe('SessionState', () => {
 
     let state;
 
@@ -65,3 +65,29 @@ describe('Session state manager', () => {
         expect(state.state).to.deep.equal(STATES.WAITING_FOR_CATEGORY);
     });
 });
+
+// since the SessionState_Alvis class is a very simple subclass of SessionState
+// we are just doing some basic sanity checking here, not a comprehensive test suite
+
+describe('SessionState_Alvis', () => {
+
+    let state
+
+    beforeEach(function() {
+        state = new SessionState_Alvis(sessionId, installationId, buttonId, unit, phoneNumber, STATES.STARTED, 1, createdAt, updatedAt, null, null);
+    })
+
+    it('should increment properly', () => {
+        state.incrementButtonPresses(1);
+        expect(state.numPresses).to.deep.equal(2);
+        state.incrementButtonPresses(2);
+        expect(state.numPresses).to.deep.equal(4);
+    })
+
+    it('should not advance the session when receiving an invalid incident category', () => {
+        state.state = STATES.WAITING_FOR_CATEGORY;
+        state.advanceSession('fff');
+        expect(state.state).to.deep.equal(STATES.WAITING_FOR_CATEGORY);
+    })
+
+})
