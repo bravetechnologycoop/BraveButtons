7cfca845d39cdf5ecb30feadd11def3084e0574d
trisapeace <theresa@brave.coop>
2020-08-11 00:58:38 +0100

    Add setup script for Pairing Tool
    Instead of documenting the fun of installing a new Raspberry Pi, we decided to script it forevermore.

---
+++ pairing-tool/setup_pi.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+if [[ $EUID > 0 ]]; then
+  echo "this script needs sudo privelages to run correctly."
+  exit 1
+else
+  # as of Raspbian Buster, this is required for wifi to work
+  rfkill unblock wifi
+
+  apt update
+  apt install -y vim nodejs npm
+
+  # install stable version of node
+  npm install -g n
+  n stable
+
+  # the node command changed location, this updates the PATH to reflect this
+  PATH="$PATH"
+
+  # install dependencies
+  su pi -c 'npm install'
+fi
