f4a7905789f937372d2f7f05870caa0654aa5141
trisapeace <theresa@brave.coop>
2020-11-05 12:50:36 -0800

    Add script to update Twilio SMS webhook URL
    - So that we can change all the SMS webhooks from
      https://chatbot.brave.coop/message to
      https://chatbot.brave.coop/alert/sms

--- server/utils.py
+++ server/utils.py
@@ -4,6 +4,8 @@ import datetime
 import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.use('Agg')
+import os
+from twilio.rest import Client as TwilioClient
 
 def send_hide_request(server_url, system_id):
     payload = {"system_id": system_id}
@@ -159,3 +161,31 @@ def plot_flic_time_series_from_server_log(log_file_url):
 
     plt.subplots_adjust(hspace=0.4)
     plt.savefig("timeseries")
+
+# Documentation: https://www.twilio.com/docs/phone-numbers/api/incomingphonenumber-resource
+def get_all_twilio_numbers(twilio_account_sid, twilio_auth_token):
+    client = TwilioClient(twilio_account_sid, twilio_auth_token)
+    twilio_numbers = client.incoming_phone_numbers.list()
+
+    return twilio_numbers
+
+# Documentation: https://www.twilio.com/docs/phone-numbers/api/incomingphonenumber-resource
+def update_twilio_sms_url(old_sms_url, new_sms_url, twilio_account_sid, twilio_auth_token):
+    twilio_numbers = get_all_twilio_numbers(twilio_account_sid, twilio_auth_token)
+
+    num_updated = 0
+    for twilio_number in twilio_numbers:
+        if (twilio_number.sms_url == old_sms_url):
+            twilio_number.update(
+                sms_url=new_sms_url
+            )
+
+            print(f"Updated {twilio_number.phone_number} {twilio_number.friendly_name}")
+
+            num_updated += 1
+
+    if num_updated == 0:
+        print('Nothing to update')
+    else:
+        print()
+        print(f'Updated {num_updated} Twilio numbers')
