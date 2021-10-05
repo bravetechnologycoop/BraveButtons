18ded21fcbe8dc42a61ad381043e7a9fdd0038db
trisapeace <theresa@brave.coop>
2021-10-05 02:43:44 -0700

    No longer update Responded At when given an invalid incident category

--- server/test/unit/BraveAlerterConfiguratorTest/alertSessionChangedCallbackTest.js
+++ server/test/unit/BraveAlerterConfiguratorTest/alertSessionChangedCallbackTest.js
@@ -12,29 +12,27 @@ const BraveAlerterConfigurator = require('../../../BraveAlerterConfigurator.js')
 // Configure Chai
 use(sinonChai)
 
+const sandbox = sinon.createSandbox()
+
 describe('BraveAlerterConfigurator.js unit tests: alertSessionChangedCallback', () => {
   beforeEach(() => {
     this.fakeCurrentTime = new Date('2020-12-25T10:09:08.000Z')
-    sinon.stub(db, 'getCurrentTime').returns(this.fakeCurrentTime)
-    sinon.stub(db, 'beginTransaction')
-    sinon.stub(db, 'getSessionWithSessionId').returns(createTestSessionState())
-    sinon.stub(db, 'getInstallationWithSessionId').returns({
+    sandbox.stub(db, 'getCurrentTime').returns(this.fakeCurrentTime)
+    sandbox.stub(db, 'beginTransaction')
+    sandbox.stub(db, 'getInstallationWithSessionId').returns({
       incidentCategories: ['Cat0', 'Cat1', 'Cat2'],
     })
-    sinon.stub(db, 'saveSession')
-    sinon.stub(db, 'commitTransaction')
+    sandbox.stub(db, 'saveSession')
+    sandbox.stub(db, 'commitTransaction')
   })
 
   afterEach(() => {
-    db.commitTransaction.restore()
-    db.saveSession.restore()
-    db.getInstallationWithSessionId.restore()
-    db.getSessionWithSessionId.restore()
-    db.beginTransaction.restore()
-    db.getCurrentTime.restore()
+    sandbox.restore()
   })
 
   it('if given alertState STARTED should update only alertState', async () => {
+    sandbox.stub(db, 'getSessionWithSessionId').returns(createTestSessionState())
+
     const sessionId = 'ca6e85b1-0a8c-4e1a-8d1e-7a35f838d7bc'
     const braveAlerterConfigurator = new BraveAlerterConfigurator()
     const braveAlerter = braveAlerterConfigurator.createBraveAlerter()
@@ -43,10 +41,12 @@ describe('BraveAlerterConfigurator.js unit tests: alertSessionChangedCallback', 
     const expectedSession = createTestSessionState()
     expectedSession.state = CHATBOT_STATE.STARTED
 
-    expect(db.saveSession).to.be.calledWith(expectedSession, sinon.any)
+    expect(db.saveSession).to.be.calledWith(expectedSession, sandbox.any)
   })
 
   it('if given alertState WAITING_FOR_REPLY should update only alertState', async () => {
+    sandbox.stub(db, 'getSessionWithSessionId').returns(createTestSessionState())
+
     const sessionId = 'ca6e85b1-0a8c-4e1a-8d1e-7a35f838d7bc'
     const braveAlerterConfigurator = new BraveAlerterConfigurator()
     const braveAlerter = braveAlerterConfigurator.createBraveAlerter()
@@ -55,10 +55,14 @@ describe('BraveAlerterConfigurator.js unit tests: alertSessionChangedCallback', 
     const expectedSession = createTestSessionState()
     expectedSession.state = CHATBOT_STATE.WAITING_FOR_REPLY
 
-    expect(db.saveSession).to.be.calledWith(expectedSession, sinon.any)
+    expect(db.saveSession).to.be.calledWith(expectedSession, sandbox.any)
   })
 
-  it('if given alertState WAITING_FOR_CATEGORY should update alertState and respondedAt', async () => {
+  it('if given alertState WAITING_FOR_CATEGORY and it has not already been responded to should update alertState and respondedAt', async () => {
+    const testSessionState = createTestSessionState()
+    testSessionState.respondedAt = null
+    sandbox.stub(db, 'getSessionWithSessionId').returns(testSessionState)
+
     const sessionId = 'ca6e85b1-0a8c-4e1a-8d1e-7a35f838d7bc'
     const braveAlerterConfigurator = new BraveAlerterConfigurator()
     const braveAlerter = braveAlerterConfigurator.createBraveAlerter()
@@ -68,10 +72,30 @@ describe('BraveAlerterConfigurator.js unit tests: alertSessionChangedCallback', 
     expectedSession.state = CHATBOT_STATE.WAITING_FOR_CATEGORY
     expectedSession.respondedAt = this.fakeCurrentTime
 
-    expect(db.saveSession).to.be.calledWith(expectedSession, sinon.any)
+    expect(db.saveSession).to.be.calledWith(expectedSession, sandbox.any)
+  })
+
+  it('if given alertState WAITING_FOR_CATEGORY and it has already been responded to should update alertState', async () => {
+    const testSessionState = createTestSessionState()
+    const testRespondedAtTime = new Date('2010-06-06T06:06:06.000Z')
+    testSessionState.respondedAt = testRespondedAtTime
+    sandbox.stub(db, 'getSessionWithSessionId').returns(testSessionState)
+
+    const sessionId = 'ca6e85b1-0a8c-4e1a-8d1e-7a35f838d7bc'
+    const braveAlerterConfigurator = new BraveAlerterConfigurator()
+    const braveAlerter = braveAlerterConfigurator.createBraveAlerter()
+    await braveAlerter.alertSessionChangedCallback(new AlertSession(sessionId, CHATBOT_STATE.WAITING_FOR_CATEGORY))
+
+    const expectedSession = createTestSessionState()
+    expectedSession.state = CHATBOT_STATE.WAITING_FOR_CATEGORY
+    expectedSession.respondedAt = testRespondedAtTime
+
+    expect(db.saveSession).to.be.calledWith(expectedSession, sandbox.any)
   })
 
   it('if given alertState WAITING_FOR_DETAILS should update only alertState', async () => {
+    sandbox.stub(db, 'getSessionWithSessionId').returns(createTestSessionState())
+
     const sessionId = 'ca6e85b1-0a8c-4e1a-8d1e-7a35f838d7bc'
     const braveAlerterConfigurator = new BraveAlerterConfigurator()
     const braveAlerter = braveAlerterConfigurator.createBraveAlerter()
@@ -80,10 +104,12 @@ describe('BraveAlerterConfigurator.js unit tests: alertSessionChangedCallback', 
     const expectedSession = createTestSessionState()
     expectedSession.state = CHATBOT_STATE.WAITING_FOR_DETAILS
 
-    expect(db.saveSession).to.be.calledWith(expectedSession, sinon.any)
+    expect(db.saveSession).to.be.calledWith(expectedSession, sandbox.any)
   })
 
   it('if given alertState COMPLETED should update only alertState', async () => {
+    sandbox.stub(db, 'getSessionWithSessionId').returns(createTestSessionState())
+
     const sessionId = 'ca6e85b1-0a8c-4e1a-8d1e-7a35f838d7bc'
     const braveAlerterConfigurator = new BraveAlerterConfigurator()
     const braveAlerter = braveAlerterConfigurator.createBraveAlerter()
@@ -92,10 +118,12 @@ describe('BraveAlerterConfigurator.js unit tests: alertSessionChangedCallback', 
     const expectedSession = createTestSessionState()
     expectedSession.state = CHATBOT_STATE.COMPLETED
 
-    expect(db.saveSession).to.be.calledWith(expectedSession, sinon.any)
+    expect(db.saveSession).to.be.calledWith(expectedSession, sandbox.any)
   })
 
   it('if given alertState and categoryKey should update alertState and category', async () => {
+    sandbox.stub(db, 'getSessionWithSessionId').returns(createTestSessionState())
+
     const sessionId = 'ca6e85b1-0a8c-4e1a-8d1e-7a35f838d7bc'
     const braveAlerterConfigurator = new BraveAlerterConfigurator()
     const braveAlerter = braveAlerterConfigurator.createBraveAlerter()
@@ -105,10 +133,12 @@ describe('BraveAlerterConfigurator.js unit tests: alertSessionChangedCallback', 
     expectedSession.state = CHATBOT_STATE.WAITING_FOR_DETAILS
     expectedSession.incidentType = 'Cat0'
 
-    expect(db.saveSession).to.be.calledWith(expectedSession, sinon.any)
+    expect(db.saveSession).to.be.calledWith(expectedSession, sandbox.any)
   })
 
   it('if given alertState and details should update alertState and details', async () => {
+    sandbox.stub(db, 'getSessionWithSessionId').returns(createTestSessionState())
+
     const sessionId = 'ca6e85b1-0a8c-4e1a-8d1e-7a35f838d7bc'
     const braveAlerterConfigurator = new BraveAlerterConfigurator()
     const braveAlerter = braveAlerterConfigurator.createBraveAlerter()
@@ -118,10 +148,12 @@ describe('BraveAlerterConfigurator.js unit tests: alertSessionChangedCallback', 
     expectedSession.state = CHATBOT_STATE.COMPLETED
     expectedSession.notes = 'fakeDetails'
 
-    expect(db.saveSession).to.be.calledWith(expectedSession, sinon.any)
+    expect(db.saveSession).to.be.calledWith(expectedSession, sandbox.any)
   })
 
   it('if given only fallback return message should update only fallback return message', async () => {
+    sandbox.stub(db, 'getSessionWithSessionId').returns(createTestSessionState())
+
     const sessionId = 'ca6e85b1-0a8c-4e1a-8d1e-7a35f838d7bc'
     const braveAlerterConfigurator = new BraveAlerterConfigurator()
     const braveAlerter = braveAlerterConfigurator.createBraveAlerter()
@@ -130,6 +162,6 @@ describe('BraveAlerterConfigurator.js unit tests: alertSessionChangedCallback', 
     const expectedSession = createTestSessionState()
     expectedSession.fallBackAlertTwilioStatus = 'fakeFallbackReturnMessage'
 
-    expect(db.saveSession).to.be.calledWith(expectedSession, sinon.any)
+    expect(db.saveSession).to.be.calledWith(expectedSession, sandbox.any)
   })
 })
