fb50851e420d021c2b526003f79beeca4a7f8f9b
trisapeace <theresa@brave.coop>
2022-02-17 15:49:50 -0800

    Reduce the frequency of heartbeat checks
    - I hope that this will stop some of postgres connection problems
      we've been having

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ if (helpers.isTestEnvironment()) {
     cert: fs.readFileSync(`/etc/letsencrypt/live/${helpers.getEnvVar('DOMAIN')}/fullchain.pem`),
   }
   server = https.createServer(httpsOptions, app).listen(443)
-  setInterval(vitals.checkHeartbeat, 1000)
+  setInterval(vitals.checkHeartbeat, 10 * 1000)
   setInterval(vitals.checkGatewayHeartbeat, 5 * 60 * 1000)
   helpers.log('brave server listening on port 443')
 }
