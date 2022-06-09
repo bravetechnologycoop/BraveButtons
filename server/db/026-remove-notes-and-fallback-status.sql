2ea5ad05e922f0c2b4f3cf7cab239eb2d75767b4
trisapeace <theresa@brave.coop>
2022-06-09 11:50:26 -0700

    wip updated SQL

--- server/db/026-remove-notes-and-fallback-status.sql
+++ server/db/026-remove-notes-and-fallback-status.sql
@@ -13,11 +13,24 @@ BEGIN
     IF migrationId - lastSuccessfulMigrationId = 1 THEN
         -- ADD SCRIPT HERE
 
+        -- Remove the notes/details column because we don't want the PII
         ALTER TABLE sessions
-        DROP COLUMN notes;
+        DROP COLUMN IF EXISTS notes;
+
+        -- Remove the fallback Twilio status column because we don't use it
+        ALTER TABLE sessions
+        DROP COLUMN IF EXISTS fallback_alert_twilio_status;
+
+        -- Update all sessions that were waiting for notes/details to now be Completed, but disable the trigger so updated_at stays the same
+        ALTER TABLE sessions
+        DISABLE TRIGGER set_sessions_timestamp;
+
+        UPDATE sessions
+        SET state = 'Completed'
+        WHERE state = 'Waiting for incident details';
 
         ALTER TABLE sessions
-        DROP COLUMN fallback_alert_twilio_status;
+        ENABLE TRIGGER set_sessions_timestamp;
 
         -- Update the migration ID of the last file to be successfully run to the migration ID of this file
         INSERT INTO migrations (id)
