92e7a2b3311b9986e06dfeae2d33622862fc927a
trisapeace <theresa@brave.coop>
2022-11-22 15:16:00 -0800

    Add US Spanish translations for chatbot

--- server/test/unit/BraveAlerterConfiguratorTest/getReturnMessageToRespondedByPhoneNumberTest.js
+++ server/test/unit/BraveAlerterConfiguratorTest/getReturnMessageToRespondedByPhoneNumberTest.js
@@ -98,4 +98,15 @@ describe('BraveAlerterConfigurator.js unit tests: getReturnMessageToRespondedByP
       "Maintenant que vous avez répondu, merci de répondre avec le numéro qui décrit le mieux l'incident:\n---\nOnce you have responded, please reply with the number that best describes the incident:\n\n0 - Cat0\n1 - Cat1\n",
     )
   })
+
+  it('should get es_us message', () => {
+    const returnMessage = this.alertStateMachine.getReturnMessageToRespondedByPhoneNumber(
+      'es_us',
+      CHATBOT_STATE.STARTED,
+      CHATBOT_STATE.WAITING_FOR_REPLY,
+      ['Cat0', 'Cat1'],
+    )
+
+    expect(returnMessage).to.equal('Una vez que haya contestado, responda con el número que mejor describa el incidente:\n0 - Cat0\n1 - Cat1\n')
+  })
 })
