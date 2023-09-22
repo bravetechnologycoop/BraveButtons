c7e41fcb9f86118b485c1b818abb3fdfa59bc7e4
ahmaddaadaa <ahmadd@uvic.ca>
2023-09-22 14:54:12 -0700

    Updated to run PostgreSQL setup script for the migration process

--- server/setup_postgresql.sh
+++ server/setup_postgresql.sh
@@ -1,12 +1,45 @@
-# Common parameters
-PG_DATABASE="$PG_USER"
-COMMON_PARAMS="-U $PG_USER -h $PG_HOST -p $PG_PORT -d $PG_DATABASE -v ON_ERROR_STOP=1 --set=sslmode=require"
+#!/bin/bash
 
-# Create PostgreSQL extension
-sudo PGPASSWORD="$PG_PASSWORD" psql $COMMON_PARAMS -c 'CREATE EXTENSION IF NOT EXISTS "pgcrypto"'
+if [[ -z "${PG_PASSWORD}" ]]; then
+  pg_password=$1
+else
+  pg_password="${PG_PASSWORD}"
+fi
+
+if [[ -z "${PG_USER}" ]]; then
+  pg_user=$2
+else
+  pg_user="${PG_USER}"
+fi
+
+if [[ -z "${PG_HOST}" ]]; then
+  pg_host=$3
+else
+  pg_host="${PG_HOST}"
+fi
+
+if [[ -z "${PG_PORT}" ]]; then
+  pg_port=$4
+else
+  pg_port="${PG_PORT}"
+fi
+
+if [[ -z "${PG_USER}" ]]; then
+  pg_dbname=$5
+else
+  pg_dbname="${PG_USER}"
+fi
+
+echo "Running PostgreSQL commands..."
+
+# Create the required PostgreSQL extensions
+sudo PGPASSWORD=$pg_password psql -U $pg_user -h $pg_host -p $pg_port -d $pg_dbname -v "ON_ERROR_STOP=1" --set=sslmode=require -c 'CREATE EXTENSION IF NOT EXISTS "pgcrypto"'
 
 # Run SQL setup scripts
 for file in $(ls -v db/*.sql); do
   echo "Running script $file"
-  sudo PGPASSWORD="$PG_PASSWORD" psql $COMMON_PARAMS -f "./$file"
+  PGPASSWORD=$pg_password psql -U $pg_user -h $pg_host -d $pg_dbname -p $pg_port -v "ON_ERROR_STOP=1" --set=sslmode=require -f ./$file
 done
+
+echo "PostgreSQL commands completed."
+
