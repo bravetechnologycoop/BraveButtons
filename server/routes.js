a1bf2799612edca200dda2b9110f59d5fca8966c
Micah Baker <micahb923@gmail.com>
2024-01-24 09:05:06 -0800

    Proposed PA API routes (reworking dashboard code)

--- server/routes.js
+++ server/routes.js
@@ -5,20 +5,38 @@ const pa = require('./pa')
 const rak = require('./rak')
 
 function configureRoutes(app) {
-  app.get('/', dashboard.sessionChecker, dashboard.redirectToHomePage)
-  app.get('/dashboard', dashboard.sessionChecker, dashboard.renderDashboardPage)
-  app.get('/clients/:clientId?', dashboard.sessionChecker, dashboard.renderClientDetailsPage)
-  app.get('/clients/:clientId/vitals', dashboard.sessionChecker, dashboard.renderClientVitalsPage)
-  app.get('/export-data', dashboard.sessionChecker, dashboard.downloadCsv)
-  app.get('/login', dashboard.renderLoginPage)
-  app.get('/logout', dashboard.submitLogout)
-  app.get('/vitals', dashboard.sessionChecker, dashboard.renderVitalsPage)
+  // clients PA API routes
+  app.get('/pa/clients', pa.validateGetClients, googleHelpers.paAuthorize, pa.handleGetClients)
+  app.post('/pa/clients', pa.validateRegisterClient, googleHelpers.paAuthorize, pa.handleRegisterClient)
+  app.get('/pa/clients/:clientId', pa.validateGetClient, googleHelpers.paAuthorize, pa.handleGetClient)
+  app.put('/pa/clients/:clientId', pa.validatePutClient, googleHelpers.paAuthorize, pa.handlePutClient)
+  app.get('/pa/clients/:clientId/buttons', pa.validateGetClientButtons, googleHelpers.paAuthorize, pa.handleGetClientButtons)
+  // app.get('/pa/clients/:clientId/sessions', pa.validateGetClientSessions, googleHelpers.paAuthorize, pa.handleGetClientSessions)
+  app.get('/pa/clients/:clientId/gateways', pa.validateGetClientGateways, googleHelpers.paAuthorize, pa.handleGetClientGateways)
 
-  app.post('/login', dashboard.submitLogin)
+  // buttons PA API routes
+  app.post('/pa/buttons', pa.validateRegisterButton, googleHelpers.paAuthorize, pa.handleRegisterButton)
+  app.get('/pa/buttons/:buttonId', pa.validateGetButton, googleHelpers.paAuthorize, pa.handleGetButton)
+  app.put('/pa/buttons/:buttonId', pa.validatePutButton, googleHelpers.paAuthorize, pa.handlePutButton)
+  app.get('/pa/buttons/:buttonId/vitals', pa.validateGetButtonVitals, googleHelpers.paAuthorize, pa.handleGetButtonVitals)
+
+  // sessions PA API routes
+  app.get('/pa/sessions/:buttonId?', pa.validateGetSessions, googleHelpers.paAuthorize, pa.handleGetSessions)
+
+  // gateways PA API routes
+  app.post('/pa/gateways', pa.validateRegisterGateway, googleHelpers.paAuthorize, pa.handleRegisterGateway)
+  app.get('/pa/gateways/:gatewayId', pa.validateGetGateway, googleHelpers.paAuthorize, pa.handleGetGateway)
+  app.put('/pa/gateways/:gatewayId', pa.validatePutGateway, googleHelpers.paAuthorize, pa.handlePutGateway)
+  app.get('/pa/gateways/:gatewayId/vitals', pa.validateGetGatewayVitals, googleHelpers.paAuthorize, pa.handleGetGatewayVitals)
+
+  // misc. PA API routes
   app.post('/pa/aws-device-registration', pa.validateAwsDeviceRegistration, googleHelpers.paAuthorize, pa.handleAwsDeviceRegistration)
   app.post('/pa/buttons-twilio-number', pa.validateButtonsTwilioNumber, googleHelpers.paAuthorize, pa.handleButtonsTwilioNumber)
   app.post('/pa/message-clients', pa.validateMessageClients, googleHelpers.paAuthorize, pa.handleMessageClients)
   app.post('/pa/health', pa.validateCheckDatabaseConnection, googleHelpers.paAuthorize, pa.handleCheckDatabaseConnection)
+
+  // other routes
+  app.get('/export-data', dashboard.sessionChecker, dashboard.downloadCsv)
   app.post('/rak_button_press', rak.validateButtonPress, rak.handleButtonPress)
 }
 
