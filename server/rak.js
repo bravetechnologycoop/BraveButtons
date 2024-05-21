21a878e7d13d69f399818191d63271797d4f5923
Elana Wood <57824556+elanawood@users.noreply.github.com>
2024-05-21 09:53:07 -0700

    Linter suggestions

--- server/rak.js
+++ server/rak.js
@@ -45,7 +45,12 @@ async function handleButtonPress(req, res) {
       if (event[0] === EVENT_TYPE.HEARTBEAT && button !== null) {
         await db.logButtonsVital(button.id, event[1], snr, rssi)
  
-      } else if (event[0] === EVENT_TYPE.BUTTON_PRESS_D || event[0] === EVENT_TYPE.BUTTON_PRESS_C || event[0]===EVENT_TYPE.BUTTON_PRESS_4 || event[0]===EVENT_TYPE.BUTTON_PRESS_3) {
+      } else if (
+         event[0] === EVENT_TYPE.BUTTON_PRESS_D ||
+         event[0] === EVENT_TYPE.BUTTON_PRESS_C || 
+         event[0] === EVENT_TYPE.BUTTON_PRESS_4 || 
+         event[0] === EVENT_TYPE.BUTTON_PRESS_3
+        ) {
 
         if (button === null) {
           const errorMessage = `Bad request to ${req.path}: DevEui is not registered: '${devEui}'`
