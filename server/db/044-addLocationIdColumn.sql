2b30fd1bd98b25456cc28610ecfbffbccef193b8
Elana Wood <elanawood13@gmail.com>
2024-05-30 13:04:29 -0700

    Add location id column to buttons table

---
+++ server/db/044-addLocationIdColumn.sql
@@ -0,0 +1,22 @@
+DO $migration$
+    DECLARE migrationId INT;
+    DECLARE lastSuccessfulMigrationId INT;
+BEGIN
+    -- The migration ID of this file
+    migrationId := 44;
+
+    -- Get the migration ID of the last file to be successfully run
+    SELECT MAX(id) INTO lastSuccessfulMigrationId
+    FROM migrations;
+
+    -- Only execute this script if its migration ID is next after the last successful migration ID
+    IF migrationId - lastSuccessfulMigrationId = 1 THEN
+
+        ALTER TABLE buttons
+        ADD COLUMN locationId SERIAL;
+
+        -- Update the migration ID of the last file to be successfully run to the migration ID of this file
+        INSERT INTO migrations (id)
+        VALUES (migrationId);
+    END IF;
+END $migration$;
