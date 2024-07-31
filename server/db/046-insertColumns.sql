0f1e6d67ab3dbc6eda4a14bc05e51f1a3e6970a6
Elana Wood <elanawood13@gmail.com>
2024-07-31 14:25:17 -0700

    PR changes!

--- server/db/046-insertColumns.sql
+++ server/db/046-insertColumns.sql
@@ -12,9 +12,9 @@ BEGIN
     -- Only execute this script if its migration ID is next after the last successful migration ID
     IF migrationId - lastSuccessfulMigrationId = 1 THEN
         -- ADD SCRIPT HERE
-        ALTER TABLE clients INSERT COLUMN Organization default text;
-        ALTER TABLE clients INSERT COLUMN Funder default text;
-        ALTER TABLE clients INSERT COLUMN PostalCode default text;
+        ALTER TABLE clients_extention INSERT COLUMN organization default text;
+        ALTER TABLE clients_extention INSERT COLUMN funder default text;
+        ALTER TABLE clients_extention INSERT COLUMN postal_code default text;
         -- Update the migration ID of the last file to be successfully run to the migration ID of this file
         INSERT INTO migrations (id)
         VALUES (migrationId);
