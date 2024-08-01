17e8aaa94990c9275ace232876049395ecdcc5e2
Elana Wood <elanawood13@gmail.com>
2024-08-01 11:17:49 -0700

    incident categories

---
+++ server/db/046-update_incident_categories.sql
@@ -0,0 +1,20 @@
+DO $migration$
+    DECLARE migrationId INT;
+    DECLARE lastSuccessfulMigrationId INT;
+BEGIN
+    -- The migration ID of this file
+    migrationId := 46;
+
+    -- Get the migration ID of the last file to be successfully run
+    SELECT MAX(id) INTO lastSuccessfulMigrationId
+    FROM migrations;
+
+    -- Only execute this script if its migration ID is next after the last successful migration ID
+    IF migrationId - lastSuccessfulMigrationId = 1 THEN
+        
+        ALTER TABLE clients ALTER COLUMN incident_categories SET DEFAULT {"Overdose Event","No One Inside","Occupant Responded","None of the Above"} ;
+        -- Update the migration ID of the last file to be successfully run to the migration ID of this file
+        INSERT INTO migrations (id)
+        VALUES (migrationId);
+    END IF;
+END $migration$;
