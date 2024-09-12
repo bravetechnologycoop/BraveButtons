a92a402aedf0dd6fb1da69ea20ae34167af4f89b
corey <corey@DESKTOP-DFRNNVU>
2024-09-12 14:16:00 -0700

    update migration 48

--- server/db/048-update-incident-categories.sql
+++ server/db/048-update-incident-categories.sql
@@ -11,7 +11,7 @@ BEGIN
 
     -- Only execute this script if its migration ID is next after the last successful migration ID
     IF migrationId - lastSuccessfulMigrationId = 1 THEN
-        ALTER TABLE clients ALTER COLUMN incident_categories SET DEFAULT '{"Overdose Event","No One Inside","Occupant Responded","None of the Above"}';
+        ALTER TABLE clients ALTER COLUMN incident_categories SET DEFAULT '{"Overdose Event","Safer Use","Conflict Resolution","Test"}';
 
         -- Update the migration ID of the last file to be successfully run to the migration ID of this file
         INSERT INTO migrations (id)
