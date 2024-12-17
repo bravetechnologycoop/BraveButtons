ea84011ba95a50e9730c04fbd1bdc77258977eea
Nakul Bansal <nakulbansal727@gmail.com>
2024-12-17 12:57:56 -0800

    Updated migration scripts
    * Combined the two scripts to single one
    * Updated commissioned at column to be first_device_live_at
    * Changed type from timestamp to date

--- server/db/051-add-status-column.sql
+++ server/db/051-add-status-column.sql
@@ -14,7 +14,13 @@ BEGIN
         -- Create ENUM for the new status column
         CREATE TYPE status_enum AS ENUM ('TESTING', 'SHIPPED', 'LIVE');
 
+        -- Add status column to clients table
+        -- Default value is set to LIVE, so current client devices are live 
         ALTER TABLE clients ADD COLUMN status status_enum NOT NULL DEFAULT 'LIVE';
+
+        -- Add first_device_live_at column to clients table
+        -- Default value is null
+        ALTER TABLE clients ADD COLUMN first_device_live_at DATE;
 
         -- Update the migration ID of the last file to be successfully run to the migration ID of this file
         INSERT INTO migrations (id)
